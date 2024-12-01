f45a7a31b7ca08cb916c828a05fe9db5e60e568d
Outlandish0191 <jmulligan@ucls.uchicago.edu>
2024-12-01 00:53:43 -0600

    e

--- cogs/stats.py
+++ cogs/stats.py
@@ -557,7 +557,7 @@ class Stats(CogU, name="Statistics", hidden=True):
         memory_usage = self.process.memory_full_info().uss / 1024**2
         cpu_usage = self.process.cpu_percent() / psutil.cpu_count()
         embed.add_field(name='Process', value=f'`{memory_usage:.2f}` MiB\n`{cpu_usage:.2f}`% CPU')
-
+ 
         version = pkg_resources.get_distribution('discord.py').version
         embed.add_field(name='Guilds', value=f"`{intcomma(guilds)}`")
         embed.add_field(name='Commands Run', value=f"`{intcomma(sum(self.bot.command_stats.values()))}`")
